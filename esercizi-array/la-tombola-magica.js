08ffea08fd2f016ead2ee1a3217a846c2b61fc98
Mattia Vena <55146023+MATVEN@users.noreply.github.com>
2022-10-06 15:19:13 +0200

    Update la-tombola-magica.js

--- esercizi-array/la-tombola-magica.js
+++ esercizi-array/la-tombola-magica.js
@@ -19,3 +19,36 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var n = 10;
+var arrUno = [];
+var arrDue = [23,73,52,42,27,2,10,67,80,39];
+
+for(var i = 0; i < n; i++){
+	num = Math.round(Math.random()*15 + 1);
+	flag = true;
+	for(var j = 0; j < arrUno.length; j++){
+	  if(arrUno[j] == num)	flag = false;
+	}
+	var controllo = flag;
+	(flag) ? arrUno[i] = num : i--;
+}
+
+var cont = 0;
+for(var i = 0; i < n; i++){
+	num = arrUno[i];
+	for(var j = 0; j < n; j++){
+		if(num == arrDue[j]){
+			cont++;
+		}
+	}
+}
+
+if(cont == 2) console.log("Ambo");
+else if(cont == 3) console.log("Terna");
+else if(cont == 4) console.log("Quaterna");
+else if(cont == 5) console.log("Cinquina");
+else if(cont == 10) console.log("Tombola");
+else console.log("Mi dispiace, hai perso!");
+
+console.log(arrUno + '\n' + arrDue + '\n' + cont);
