6ce37a436db73a5eaab2a7320386aa1ad8ba55ad
Mattia Vena <55146023+MATVEN@users.noreply.github.com>
2022-10-07 18:29:30 +0200

    Update le-tabelline-nel-tabellone.js

--- esercizi-array-multidimensionali/le-tabelline-nel-tabellone.js
+++ esercizi-array-multidimensionali/le-tabelline-nel-tabellone.js
@@ -31,20 +31,13 @@ console.log(arr + '\n' + res.join('\n'));
 
 /* METODO 2 */
 
-var arr = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]];
+var matrice = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]];
 var tabellone = [];
 
+for(var i = 0; i < matrice.length; i++){
+  for(var j = 0; j < matrice[i].length; j++){
+    
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(tabellone);
