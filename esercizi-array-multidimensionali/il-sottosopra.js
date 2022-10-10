a9ad4b3bb7fa02c0b7f29bb9542b12b060e310d3
Mattia Vena <55146023+MATVEN@users.noreply.github.com>
2022-10-10 22:17:16 +0200

    Update il-sottosopra.js

--- esercizi-array-multidimensionali/il-sottosopra.js
+++ esercizi-array-multidimensionali/il-sottosopra.js
@@ -17,3 +17,17 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var array = [[1, 2], [3, 4], [5, 6]];
+var arrM = [];
+
+for(let i = 0; i < array.length; i++){
+  for(let j = 0; j < array[i].length; j++){
+    if (arrM[j] === undefined)
+    arrM[j] = [];
+    arrM[j][i] = array[i][j];
+  }
+}
+
+console.log(array);
+console.log(arrM);
