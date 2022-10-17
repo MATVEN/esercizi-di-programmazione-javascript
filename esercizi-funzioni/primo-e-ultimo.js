87702fa406b542094a0af7082f10dc4b29e848f4
Mattia Vena <55146023+MATVEN@users.noreply.github.com>
2022-10-17 12:27:19 +0200

    Update primo-e-ultimo.js

--- esercizi-funzioni/primo-e-ultimo.js
+++ esercizi-funzioni/primo-e-ultimo.js
@@ -13,3 +13,26 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var a = Math.round(Math.random()*100);
+var b = Math.round(Math.random()*100);
+var c = Math.round(Math.random()*100);
+	
+var grande = maggiore(a,b,c);
+var piccolo = minore(a,b,c)
+	
+function maggiore(a,b,c){
+  var max = a;
+	if(max < b) max = b;
+	if(max < c) max = c;
+	return max;
+}
+
+function minore(a,b,c){
+  var min = a;
+	if(min > b) min = b;
+	if(min > c) min = c;
+	return min;
+}
+	
+console.log(grande, piccolo);
