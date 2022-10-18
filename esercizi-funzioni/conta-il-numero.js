2e28076253fedf7d2d20d5de344883f5a3a0a518
Mattia Vena <55146023+MATVEN@users.noreply.github.com>
2022-10-18 17:01:05 +0200

    Update conta-il-numero.js

--- esercizi-funzioni/conta-il-numero.js
+++ esercizi-funzioni/conta-il-numero.js
@@ -12,3 +12,16 @@
     
   http://www.imparareaprogrammare.it
 */
+var number = Math.round(Math.random()*9999 + 1);
+console.log(number)
+
+function contaCifre(number){
+  var length = (number + '').replace('.', '').replace('-', '').length;
+  if(length == 1){
+    return length + ' cifra';
+  }else{
+    return length + ' cifre';
+  }
+}
+
+console.log(contaCifre(number));
